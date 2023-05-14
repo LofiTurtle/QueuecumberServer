81ee84661744296c2fc90313deb64e154850b197
sohinibagchi <you@example.com>
2023-05-13 19:46:16 -0700

    create_listening_sessions update

--- server/utils/listeningsession.py
+++ server/utils/listeningsession.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
-from server.database.datamanager import get_user_listening_history, save_listening_session
+from server.database.datamanager import get_user_listening_history, save_listening_session, get_latest_listening_session
 
 
 def create_listening_sessions(spotify_user_id: str) -> None:
+    latest_listening_session = get_latest_listening_session(spotify_user_id)
     history = get_user_listening_history(spotify_user_id)
     start_time = -1
     previous_played_at = history[0].played_at
@@ -13,16 +14,21 @@ def create_listening_sessions(spotify_user_id: str) -> None:
         if start_time == -1:
             start_time = history[i].played_at
 
+            while latest_listening_session.end_time >= start_time:
+                i += 1
+                start_time = history[i].played_at
+                previous_played_at = history[i].played_at
+
         current_played_at = history[i].played_at
         time_difference = current_played_at - previous_played_at
 
-        if time_difference.total_seconds() >= 1800:
+        if time_difference.total_seconds() >= 1800 and i < len(history) - 1:
             save_listening_session(spotify_user_id, start_time, previous_played_at)
             start_time = current_played_at
 
-        if i == (len(history)) - 1:
-            save_listening_session(spotify_user_id, start_time, previous_played_at)
-            start_time = -1
+        if i == len(history) - 1:
+            if time_difference.total_seconds() >= 1800:
+                save_listening_session(spotify_user_id, start_time, previous_played_at)
             #print("done")
 
         previous_played_at = current_played_at
